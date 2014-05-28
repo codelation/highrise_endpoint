5d90879bedc096a78b0f8e628a172f37cd4e4014
Matthew Sullivan <msull92@gmail.com>
2014-05-28 15:39:32 -0500

    add spec framework

---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+require "bundler"
+require "pstore"
+
+# require 'spree/testing_support/controllers'
+
+Bundler.require(:default, :test)
+
+# ENV['ENDPOINT_KEY'] = 'x123'
+
+require File.join(File.dirname(__FILE__), '..', 'highrise_endpoint')
+Dir["./spec/support/**/*.rb"].each {|f| require f}
+
+Sinatra::Base.environment = 'test'
+
+def app
+  HighriseEndpoint
+end
+
+VCR.configure do |c|
+  # c.allow_http_connections_when_no_cassette = true
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  # config.include Spree::TestingSupport::Controllers
+end
