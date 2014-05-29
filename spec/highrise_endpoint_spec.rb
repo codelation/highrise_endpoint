e35002b649c502d9b97aac4bebb9723c11752c56
timwhit123 <timothy.whitney@my.und.edu>
2014-05-29 12:54:02 -0500

    Created add_order spec

--- spec/highrise_endpoint_spec.rb
+++ spec/highrise_endpoint_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe HighriseEndpoint do
   let(:add_customer) { JSON.parse(IO.read("#{File.dirname(__FILE__)}/support/requests/add_customer.json")).with_indifferent_access }
+  let(:add_order)    { JSON.parse(IO.read("#{File.dirname(__FILE__)}/support/requests/add_order.json")).with_indifferent_access }
 
   describe "| POST -> '/add_customer'" do
     before(:each) do
@@ -43,6 +44,44 @@ describe HighriseEndpoint do
 
     it "should return the webhook request_id" do
       @response_body[:request_id].should eql add_customer[:request_id]
+    end
+  end
+
+  describe "| POST -> /add_order" do
+    before(:each) do
+      VCR.use_cassette(:add_order) do
+        post '/add_order', add_customer.to_json, auth
+      end
+      @response_body = JSON.parse(last_response.body).with_indifferent_access
+    end
+
+    it "should return 200" do
+      last_response.status.should eql 200
+    end
+
+    it "should add information to Highrise" do
+      VCR.use_cassette(:retrieve_person) do
+        order = add_order[:order]
+
+        deals = Highrise::Deal.search(
+          value:     order[:id]
+          createdat: order[:placed_on]
+          currency:  order[:currency]
+          firstname: order[:billing_address][:firstname]
+          lastname:  order[:billing_address][:lastname]
+          price:     order[:totals][:order]
+        )
+
+        deals.length.should eql 1
+      end
+    end
+
+    it "should return a nice summary" do
+      @response_body[:summary].should eql "Order was added to Highrise"
+    end
+
+    it "should return the webhook request_id" do
+      @response_body[:request_id].should eql add_order[:request_id]
     end
   end
 end
