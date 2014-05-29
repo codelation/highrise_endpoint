87015b2dc1227c6253dfc49b2b6ee62b1dac390b
timwhit123 <timothy.whitney@my.und.edu>
2014-05-29 13:49:23 -0500

    Added shipment specs

--- spec/highrise_endpoint_spec.rb
+++ spec/highrise_endpoint_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe HighriseEndpoint do
   let(:add_customer) { JSON.parse(IO.read("#{File.dirname(__FILE__)}/support/requests/add_customer.json")).with_indifferent_access }
+  let(:add_shipment) { JSON.parse(IO.read("#{File.dirname(__FILE__)}/support/requests/add_customer.json")).with_indifferent_access }
 
   describe "| POST -> '/add_customer'" do
     before(:each) do
@@ -43,6 +44,48 @@ describe HighriseEndpoint do
 
     it "should return the webhook request_id" do
       @response_body[:request_id].should eql add_customer[:request_id]
+    end
+  end
+
+  describe "| POST -> '/add_shipment" do
+    before(:each) do
+      VCR.use_cassette(:add_person) do
+        post "/add_shipment", add_shipment.to_json, auth
+      end
+
+      @response_body = JSON.parse(last_response.body).with_indifferent_access
+    end
+
+    it "should return 200" do
+      last_response.status.should eql 200
+    end
+
+    it "should add information to Highrise" do
+      VCR.use_cassette(:retrieve_person) do
+        shipment = add_shipment[:shipment]
+        item = add_shipment[:item]
+        shipping_address = add_shipment[:shipping_address]
+        deal = Highrise::Deal.search(
+          value: shipment[:order_id] #retrieving the deal's spree-order id
+        )
+
+        shipments = Highrise::Note.search(
+          subject_id: deal[:id],
+          subject_type: 'Deal',
+          created_at: deal[:shipped_at],
+          body: "#{item[:quantity]} #{pluralize(item[:quantity], "item")} shipped to 
+          #{shipping_address[:firstname]} #{shipping_address[:lastname]}"
+        )
+        shipments.length.should eql 1
+      end
+    end
+
+    it "should return a nice summary" do
+      @response_body[:summary].should eql "Shipment was added to Highrise"
+    end
+
+    it "should return the webhook request_id" do
+      @response_body[:request_id].should eql add_shipment[:request_id]
     end
   end
 end
