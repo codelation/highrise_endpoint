6bb34d2fed3242fda9e505d7e9e48700a4e6c379
Matthew Sullivan <msull92@gmail.com>
2014-05-29 09:52:54 -0500

    refactor rspec

--- spec/highrise_endpoint_spec.rb
+++ spec/highrise_endpoint_spec.rb
@@ -2,19 +2,21 @@ require 'spec_helper'
 
 describe HighriseEndpoint do
   let(:add_customer) { JSON.parse(IO.read("#{File.dirname(__FILE__)}/support/requests/add_customer.json")).with_indifferent_access }
-  
+
   describe "| POST -> '/add_customer'" do
-    it "should return 200" do
+    before(:each) do
       VCR.use_cassette(:add_person) do
         post '/add_customer', add_customer.to_json, auth
-        last_response.status.should eql 200
       end
+
+      @response_body = JSON.parse(last_response.body).with_indifferent_access
+    end
+
+    it "should return 200" do
+      last_response.status.should eql 200
     end
 
     it "should add information to Highrise" do
-      VCR.use_cassette(:add_person) do
-        post '/add_customer', add_customer.to_json, auth
-      end
       VCR.use_cassette(:retrieve_person) do
         customers = Highrise::Person.search(
           email: add_customer[:customer][:email],
@@ -26,19 +28,11 @@ describe HighriseEndpoint do
     end
 
     it "should return a nice summary" do
-      VCR.use_cassette(:add_person) do
-        post '/add_customer', add_customer.to_json, auth
-        response = JSON.parse(last_response.body).with_indifferent_access
-        response[:summary].should eql "Customer was added to Highrise as a person."
-      end
+      @response_body[:summary].should eql "Customer was added to Highrise."
     end
-  
+
     it "should return the webhook request_id" do
-      VCR.use_cassette(:add_person) do
-        post '/add_customer', add_customer.to_json, auth
-        response = JSON.parse(last_response.body).with_indifferent_access
-        response[:request_id].should eql add_customer[:request_id]
-      end
+      @response_body[:request_id].should eql add_customer[:request_id]
     end
   end
 end
