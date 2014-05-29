11601ba570217d0c715b6f2fd365920392b0f293
ShahanaMay <shahana@codelation.com>
2014-05-29 10:11:40 -0500

    add necessary spree information to spec

--- spec/highrise_endpoint_spec.rb
+++ spec/highrise_endpoint_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe HighriseEndpoint do
   let(:add_customer) { JSON.parse(IO.read("#{File.dirname(__FILE__)}/support/requests/add_customer.json")).with_indifferent_access }
-  
+
   describe "| POST -> '/add_customer'" do
     it "should return 200" do
       VCR.use_cassette(:add_person) do
@@ -20,6 +20,12 @@ describe HighriseEndpoint do
           email: add_customer[:customer][:email],
           firstname: add_customer[:customer][:firstname],
           lastname: add_customer[:customer][:lastname]
+          number: add_customer[:customer][:billing_address][:phone]
+          street: add_customer[:customer][:billing_address][:address1]
+          city: add_customer[:customer][:billing_address][:city]
+          state: add_customer[:customer][:billing_address][:state]
+          zip: add_customer[:customer][:billing_address][:zipcode]
+          country: add_customer[:customer][:billing_address][:country]
         )
         customers.length.should eql 1
       end
@@ -32,7 +38,7 @@ describe HighriseEndpoint do
         response[:summary].should eql "Customer was added to Highrise as a person."
       end
     end
-  
+
     it "should return the webhook request_id" do
       VCR.use_cassette(:add_person) do
         post '/add_customer', add_customer.to_json, auth
