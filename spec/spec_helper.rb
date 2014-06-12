6eedd22a21dc775423d6668628a3573f606bce70
Matthew Sullivan <msull92@gmail.com>
2014-06-12 11:39:29 -0500

    fix require order:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ Dotenv.load
 
 require File.join(File.dirname(__FILE__), '..', "highrise_endpoint")
 Dir["./spec/support/**/*.rb"].each { |f| require f }
+require "./lib/blueprint"
 Dir["./lib/**/*.rb"].each { |f| require f }
 require "spree/testing_support/controllers"
 
